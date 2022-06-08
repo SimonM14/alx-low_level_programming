be0b070288297c039f94c9285d392bd722ebd920
SimonM14 <kaninisimon79@gmail.com>
2022-06-08 10:35:29 +0000

    Added new file with task 0

---
+++ 0x00-hello_world/bettys.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+  
+a=0;
+while (a < b)
+  printf("%d\n", a++)
+    
+}
---
+++ 0x01-variables_if_else_while/hello.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+
+/*Prints a simple message hello*/
+
+void main()
+{
+  printf("hELLO");
+}
