c600630f17793a160151685e169bc0a3b2828a22
SimonM14 <kaninisimon79@gmail.com>
2022-07-04 17:29:07 +0000

    Added task 0 to task 4

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct_dog - a struct named dog
+ *
+ *@name: first member
+ *@age: second member
+ *@owner: third member
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+
+/**
+ *dog_t -  typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
